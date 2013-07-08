d3ead43605ffb7598f18123a14540fc02e3caf66
Mikhail Voronin <mvoronin.programmer@gmail.com>
2013-07-09 01:38:19 +0600

    Disable sites (admin.py)

--- django_comments/admin.py
+++ django_comments/admin.py
@@ -21,7 +21,7 @@ class UsernameSearch(object):
 class CommentsAdmin(admin.ModelAdmin):
     fieldsets = (
         (None,
-           {'fields': ('content_type', 'object_pk', 'site')}
+           {'fields': ('content_type', 'object_pk')}
         ),
         (_('Content'),
            {'fields': ('user', 'user_name', 'user_email', 'user_url', 'comment')}
@@ -32,7 +32,7 @@ class CommentsAdmin(admin.ModelAdmin):
      )
 
     list_display = ('name', 'content_type', 'object_pk', 'ip_address', 'submit_date', 'is_public', 'is_removed')
-    list_filter = ('submit_date', 'site', 'is_public', 'is_removed')
+    list_filter = ('submit_date', 'is_public', 'is_removed')
     date_hierarchy = 'submit_date'
     ordering = ('-submit_date',)
     raw_id_fields = ('user',)
@@ -83,5 +83,5 @@ class CommentsAdmin(admin.ModelAdmin):
 
 # Only register the default admin if the model is the built-in comment model
 # (this won't be true if there's a custom comment app).
-if get_model() is Comment:
-    admin.site.register(Comment, CommentsAdmin)
+#if get_model() is Comment:
+#    admin.site.register(Comment, CommentsAdmin)
